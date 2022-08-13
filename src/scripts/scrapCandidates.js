b6451a88e67fb575e641e8543a7c61c94a2d1e70
LuisIzarra <lrodriguezizarra@gmail.com>
2022-08-12 19:38:42 -0500

    first commit

--- src/scripts/scrapCandidates.js
+++ src/scripts/scrapCandidates.js
@@ -1,53 +1,66 @@
-import { searchSelectors } from '../config/scrapper.config';
-import AxiosService from '../service/axiosService';
-import { $$, $ } from '../utils/selectors';
-import { waitForScroll, waitForSelector } from '../utils/waitFor';
+import { searchSelectors } from "../config/scrapper.config";
+import axiosService from "../service/axiosService";
+import AxiosService from "../service/axiosService";
+import { $$, $ } from "../utils/selectors";
+import { waitForScroll, waitForSelector } from "../utils/waitFor";
 
 // eslint-disable-next-line no-unused-vars
-async function init () {
+async function init() {
   await waitForSelector(searchSelectors.paginateResultsContainer);
 
-  await waitForScroll(100,100);
+  await waitForScroll(100, 100);
 
-  const URLsCandidates = $$(searchSelectors.paginateResultsContainer)
-    .map(element=>$('.app-aware-link', element).href);
-  
+  const URLsCandidates = $$(searchSelectors.paginateResultsContainer).map(
+    (element) => $(".app-aware-link", element).href
+  );
 
   // eslint-disable-next-line no-undef
-  const port = chrome.runtime.connect({ name: 'secureChannelScrap' });
+  const port = chrome.runtime.connect({ name: "secureChannelScrap" });
 
   port.postMessage({ URLsCandidates });
 }
 
 // eslint-disable-next-line no-unused-vars
-async function initV2(keywords = 'fullstack', startPaginate = 0) {
+async function initV2(keywords = "fullstack", startPaginate = 0) {
   let pagination = startPaginate;
   let urlsCandidates = [];
 
-  do{
-    const { included } = await AxiosService
-      .getPaginate10Results(keywords, pagination);
-  
-    const nextCandidates = included
-      ?.filter(includedElement=> includedElement?.trackingUrn)
-      .map(filteredIncluded => {
-        const raw = filteredIncluded?.navigationContext?.url;
-        const [profileVar] = raw.match(/urn.+/) ?? [];
-        return {
-          raw,
-          profileVar: profileVar.replace('miniP','p').replace('Afs','Afsd')
-        }; 
-      }) ?? [];
-  
-    urlsCandidates = [...urlsCandidates, ...nextCandidates ];
+  do {
+    const { included } = await AxiosService.getPaginate10Results(
+      keywords,
+      pagination
+    );
 
-    pagination+=10;
+    const nextCandidates =
+      included
+        ?.filter((includedElement) => includedElement?.trackingUrn)
+        .map((filteredIncluded) => {
+          const raw = filteredIncluded?.navigationContext?.url;
+          const nameCandidate = $(
+            `.entity-result__content a[href="${raw}"] span span`
+          )?.textContent;
+          const experienceCandidate = $(
+            "#main > div > div > div:nth-child(2) > ul > li:nth-child(1) > div > div > div.entity-result__content.entity-result__divider.pt3.pb3.t-12.t-black--light > div.mb1 > div.linked-area.flex-1.cursor-pointer > div > div.entity-result__primary-subtitle.t-14.t-black.t-normal"
+          ).textContent;
+          const [profileVar] = raw.match(/urn.+/) ?? [];
+          // const nameCandidate = await axiosService.getProfileInfo(profileVar);
+          return {
+            raw,
+            nameCandidate,
+            experienceCandidate,
+            profileVar: profileVar.replace("miniP", "p").replace("Afs", "Afsd"),
+          };
+        }) ?? [];
 
-  // TO-DO: encontrar el total o el max de paginacion en la res de la query
-  }while(pagination<50);
+    urlsCandidates = [...urlsCandidates, ...nextCandidates];
+
+    pagination += 10;
+
+    // TO-DO: encontrar el total o el max de paginacion en la res de la query
+  } while (pagination < 50);
 
   // eslint-disable-next-line no-undef
-  const port = chrome.runtime.connect({ name: 'secureChannelScrapV2' });
+  const port = chrome.runtime.connect({ name: "secureChannelScrapV2" });
   port.postMessage({ urlsCandidates });
 
   return urlsCandidates;
